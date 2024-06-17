b0e8446edd5df72b17c7b51846ae5c4249fbb38a
Murad Lalayev <lalayevmurad@gmail.com>
2024-06-17 23:01:12 +0400

    face detection done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import Webcam from "react-webcam";
-// import { drawMesh } from "./utilities";
+import { drawMesh } from "./utilities";
 import * as facemesh from "@tensorflow-models/facemesh";
 
 
 function App() {
+
+  const [mirrored, setMirrored] = useState(true);
 
   // Setup references
   const webcamRef = useRef(null);
@@ -44,7 +46,10 @@ function App() {
       // Make detections
       const face = await net.estimateFaces(video);
       console.log(face);
+
       // Get canvas context for drawing (optional)
+      const ctx = canvasRef.current.getContext("2d");
+      requestAnimationFrame(() => { drawMesh(face, ctx) });
     }
   }
 
@@ -55,29 +60,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Webcam ref={webcamRef} style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zIndex: 9,
-          width: 640,
-          height: 480
-        }} />
+        <Webcam ref={webcamRef}
+          // mirrored={mirrored}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }} />
 
-        <canvas ref={canvasRef} style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zIndex: 9,
-          width: 640,
-          height: 480
-        }} />
+        <canvas
+          mirrored={mirrored}
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }} />
       </header>
     </div>
   );
